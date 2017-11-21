ce9dc005af3164acf86568ccff24fc9993effdb7
mpracek <mpracek299@gmail.com>
2017-11-22 00:33:18 +0100

    Začel z urejanjem virov ter datoteke uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -59,25 +59,43 @@ require(readr)
 velikost_slovenskih_vinogradov = read.csv2("velikost_vinogradov.csv")
 #velikosti vinogradov in število trt 2000 in 2010
 velikost.stevilo_trt =read.csv2("velikost_vinogradov_število_trt.csv")
-#pridelovalci 
-pridelovalci = read.csv2(
+#pridelovalci po statističnih regijah
+pridelovalci_regije = read.csv2(
   "Pridelovalci, površina, Število vinogradov in število sadik glede na sedež kmetijskega gospodarstva,statistične regije, Slovenija, 2009 in 2015")
+#pridelovalci po površini
+pridelovalci_povrsina =read.csv2("razredi_pridelovalcev_2009_2015.csv")
 #površina in število vinogradov glede na nagib vertikal po vinorodnih deželah in okoliših
 vertikale = read.csv2("Povšina_nagib_vertikal.csv")
 #površina, število vinogradov in sadik glede na zatravljenost
 zatravljenost = read.csv2("Površina_zatravljenost.csv")
 #površina, število vinogradov in sadik 2009, 2015
-povrsina.stevilo_vinogradov.sadik = read.csv2(
-  "Površina, število vinogradov in število sadik po VINORODNA DEŽELA IN OKOLIŠ, VELIKOSTNI RAZREDI, LETO , MERITVE.csv")
+povrsina.stevilo_vinogradov.sadik = read.csv2("
+  Površina, število vinogradov in število sadik po VINORODNA DEŽELA IN OKOLIŠ, VELIKOSTNI RAZREDI, LETO , MERITVE.csv")
 #podlaga na kateri rastejo trte 2009 in 2015
 podlaga = read.csv2("Površina_podlaga_2009_2015")
 #način gojenja trt
 gojenje = read.csv2("Površina_gojitvena_oblika_2009_2015")
-
 #ekološka pridelava (primerjava)
 ekolosko = read.csv2("Ekološka_pridelava.csv")
 #površina in število trsnic, matičnjakov in vinogradov (primerjava)
 trsnice.maticnjaki = read.csv2("trsnice_matičnjaki_vinogradi_regije_2009.csv")
+
+#Drugi del
+
+#Najpogostejše sorte po površini, številu sadik
+sorte.povrsina_sadike = read.csv2("
+                                 Povšina vinogradov in število sadik najpogostejših sort, vinorodne dežele, Slovenija, 2009 in 2015.csv")
+#Starost trt po površini in številu sadik
+starost.povrsin_sadike = read.csv2("Površina vinogradov in število sadik po starostnih razredih, vinorodne dežele in okoliši, Slovenija, 2009 in 2015")
+#Najpogostejše sorte po starosti
+sorte_starost = read.csv2("Površina_najpogostejše_sorte_starost_2009_2015.csv")
+#Pridelava grozdja
+grozdje = read.csv2("pridelava_grozdja2000-2016.csv")
+#Pridelava po količini 2009
+kolicina = read.csv2("pridelovalci_količina.csv")
+#vpeljava naših domačih sort
+#branje iz spletne strani....???
+                                 
 
 
 
